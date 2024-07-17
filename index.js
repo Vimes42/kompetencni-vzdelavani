620d580c0973ada368f29f2348bf6491d3a19534
Lenka Polcerova <lenka.polcerova@gmail.com>
2024-07-17 23:12:45 +0200

    styly tabulky, vlastností a dalsi drobnosti

--- index.js
+++ index.js
@@ -1,5 +1,70 @@
 console.log("ok")
 
+const characteristicsDiv = (heading, text) => {
+    const div = document.createElement('div');
+    div.classList.add('characteristics__char');
+
+    const h3 = document.createElement('h3');
+    h3.textContent = heading;
+    div.appendChild(h3);
+
+    const img2 = document.createElement('img');
+    img2.src = '/img/arrow-down-sign-navigate_32195.png';
+    img2.classList.add('icon_more');
+    div.appendChild(img2);
+
+    const p = document.createElement('p');
+    p.textContent = text;
+    div.appendChild(p);
+
+    div.addEventListener('click', () => {
+        p.classList.toggle('show');
+    });
+
+    return div;
+};
+
+
+const chars = [
+    {
+        heading: 'Zaměření na kompetence',
+        text: 'Kompetenční vzdělávání se zaměřuje na rozvoj znalostí, dovedností a postojů, které studentstvo potřebuje pro život a práci. Nejde jen o předávání znalostí a faktů.'
+    },
+    {
+        heading: 'Aktivní a individuální učení',
+        text: 'Studentstvo se učí aktivně a individuálně. Je zodpovědné za svůj vlastní proces učení, učitelky a učitelé jim v tom pomáhají.'
+    },
+    {
+        heading: 'Průběžné a formativní hodnocení',
+        text: 'Hodnocení je průběžné a formativní. Studenti dostávají zpětnou vazbu v průběhu celého procesu učení a mohou se tak neustále zlepšovat.'
+    },
+    {
+        heading: 'Modulární a flexibilní obsah',
+        text: 'Obsah je modulární a flexibilní. Studenti si mohou vybrat moduly, které je zajímají a které jim pomohou dosáhnout jejich cílů.'
+    },
+    {
+        heading: 'Individuální tempo',
+        text: 'Studenti se učí svým vlastním tempem. Neexistuje žádné jednotné tempo pro všechny.'
+    },
+    {
+        heading: 'Učitel jako facilitátor',
+        text: 'Učitel je facilitátor a průvodce. Pomáhá studentům učit se a rozvíjet své kompetence.'
+    },
+    {
+        heading: 'Zaměření na studenta',
+        text: 'Učení je zaměřeno na studenta. Učitel vychází z potřeb a zájmů studenta.'
+    },
+    
+];
+
+const characteristicsDivHTML = document.querySelector('.characteristics__div');
+chars.forEach(char => {
+    characteristicsDivHTML.appendChild(characteristicsDiv(char.heading, char.text));
+});
+
+
+
+/* img2.classList.add('icon_more');*/
 /* const characteristicsDiv = (heading, text) => {
     characteristicsDiv.addEventListener("onmouse", () => {
         const characteristicsDivHTML = document.querySelector(".characteristics__div");
@@ -33,14 +98,9 @@ characteristicsDivHTML = characteristicsDiv(chars.heading, chars.text);*/
 
 /*
             <div intro class="characteristics__char2"><img src="/img/check_157977.png" alt="icon_check" class="icon_check">: </div>
-            <div intro class="characteristics__char3"><img src="/img/check_157977.png" alt="icon_check" class="icon_check">Průběžné a formativní hodnocení: Hodnocení je průběžné a formativní.
-                Studenti dostávají zpětnou vazbu v průběhu celého procesu učení a mohou
-                se tak neustále zlepšovat.</div>
-            <div intro class="characteristics__char4"><img src="/img/check_157977.png" alt="icon_check" class="icon_check">Modulární a flexibilní obsah: Obsah je modulární a flexibilní. Studenti si
-                mohou vybrat moduly, které je zajímají a které jim pomohou dosáhnout jejich cílů.</div>
-            <div intro class="characteristics__char5"><img src="/img/check_157977.png" alt="icon_check" class="icon_check">Individuální tempo: Studenti se učí svým vlastním tempem. Neexistuje
-                žádné jednotné tempo pro všechny.</div>
-            <div intro class="characteristics__char6"><img src="/img/check_157977.png" alt="icon_check" class="icon_check">Učitel jako facilitátor: Učitel je facilitátor a průvodce. Pomáhá studentům učit
-                se a rozvíjet své kompetence.</div>
+            <div intro class="characteristics__char3"><img src="/img/check_157977.png" alt="icon_check" class="icon_check"></div>
+            <div intro class="characteristics__char4"><img src="/img/check_157977.png" alt="icon_check" class="icon_check"></div>
+            <div intro class="characteristics__char5"><img src="/img/check_157977.png" alt="icon_check" class="icon_check">
+            <div intro class="characteristics__char6"><img src="/img/check_157977.png" alt="icon_check" class="icon_check">
             <div intro class="characteristics__char7"><img src="/img/check_157977.png" alt="icon_check" class="icon_check">Zaměření na studenta: Učení je zaměřeno na studenta. Učitel vychází z
                 potřeb a zájmů studenta.</div>*/
